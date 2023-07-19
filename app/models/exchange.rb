253e54a0c5cfdac83d2f1512b3e1582bfc8581d4
getssh <gtamrat33@gmail.com>
2023-07-20 00:06:53 +0300

    Add associations to models

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,2 +1,4 @@
 class Exchange < ApplicationRecord
+  belongs_to :user, foreign_key: :author_id
+  has_and_belongs_to_many :categories
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  has_many :exchanges, foreign_key: :author_id
 end
