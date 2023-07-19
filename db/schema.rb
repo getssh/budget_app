e5a0c8ea53b9f961cc905f2230c0944f328080c5
getssh <gtamrat33@gmail.com>
2023-07-20 00:13:25 +0300

    Add migration file for exchanges and categories joint table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_19_204808) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_19_211005) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_19_204808) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_exchanges", id: false, force: :cascade do |t|
+    t.bigint "exchange_id", null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id", "exchange_id"], name: "index_categories_exchanges_on_category_id_and_exchange_id"
+    t.index ["exchange_id", "category_id"], name: "index_categories_exchanges_on_exchange_id_and_category_id"
   end
 
   create_table "exchanges", force: :cascade do |t|
