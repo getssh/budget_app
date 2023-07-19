93f40d004a5a79b158658212631663d791672ff6
getssh <gtamrat33@gmail.com>
2023-07-19 23:52:39 +0300

    Generate user, exchange and categroy and related files

---
+++ app/views/exchanges/_exchange.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! exchange, :id, :author_id, :name, :amount, :created_at, :updated_at
+json.url exchange_url(exchange, format: :json)
---
+++ db/migrate/20230719204659_create_exchanges.rb
@@ -0,0 +1,11 @@
+class CreateExchanges < ActiveRecord::Migration[7.0]
+  def change
+    create_table :exchanges do |t|
+      t.integer :author_id
+      t.string :name
+      t.decimal :amount
+
+      t.timestamps
+    end
+  end
+end
